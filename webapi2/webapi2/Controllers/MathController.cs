1febb947877ee52230a7782f5afbe6bd871266e8
srinivasv18 <srinivasv18@gmail.com>
2022-06-23 07:58:17 +0530

    june 23 changes

---
+++ webapi2/webapi2/Controllers/MathController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webapi2.Controllers
+{
+    [Route("api/Math")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        // GET: api/<MathController1>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet("Sum")]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        // GET api/<MathController1>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MathController1>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MathController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MathController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ webapi2/webapi2/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace webapi2.Controllers
+{
+    [Route("api/[Values]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("/getvalues")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+       
+    }
+}
