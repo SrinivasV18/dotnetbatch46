ed42c1fea0382836c49459f2b90b8d41218f6096
srinivasv18 <srinivasv18@gmail.com>
2022-07-04 08:19:39 +0530

    jul4

---
+++ Asp.net Core Web app1/Pages/Contacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asp.net_Core_Web_app1.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
